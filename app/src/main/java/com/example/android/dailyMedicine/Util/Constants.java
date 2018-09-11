10fcb1870d00498f5482a78a2c07a201bd103d12
Abdel-Rhman ALi <abdelrahman.ali.9400@gmail.com>
2018-09-11 21:05:34 +0200

    Add files via upload

--- app/src/main/java/com/example/android/dailyMedicine/Util/Constants.java
+++ app/src/main/java/com/example/android/dailyMedicine/Util/Constants.java
@@ -1,25 +1,17 @@
-package com.example.android.dailyMedicine.Util;
-
-/**
- * Created by Abdel-Rhman on 3/9/2017.
- */
+package com.example.android.dailyMedicine.util;
 
 public final class Constants {
 
+    private static final String APP_PACKAGE_NAME = "com.example.android.dailyMedicine";
     public static final String MEDICINE_TABLE_NAME = "medicine";
-
-    public static final String APP_PACKAGE_NAME = "com.example.android.dailyMedicine";
-
+    public static final String DATABASE_NAME = "dailyMedicine.db";
     public static final String ACTION_BOOT_COMPLETED = "android.intent.action.BOOT_COMPLETED";
-
     public final static String ACTION_NOTIFICATION = APP_PACKAGE_NAME + ".ACTION_NOTIFICATION";
-
     public static final String MEDICINE_ID_EXTRA = "medicine_id";
-
+    public static final String MEDICINE_NAME_EXTRA = "medicine_name";
+    public static final String MEDICINE_TAKE_TIMES_EXTRA = "medicine_take_times";
     public static final String NOTIFICATION_CHANNEL_ID = "DailyMedicineNCI";
-
     public final static String ACTION_TOOK_IT = APP_PACKAGE_NAME + ".ACTION_TOOK_IT";
-
     public final static String ACTION_MISSED_IT = APP_PACKAGE_NAME + ".ACTION_MISSED_IT";
 
 }
